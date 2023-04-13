aa5438a8ec92842b7d3a7879c99fac6f42565e9d
wushusuoshuweishu <djk1160708111@gmail.com>
2023-04-13 16:27:09 +0800

    auto answer bug fix

--- run.py
+++ run.py
@@ -1,6 +1,6 @@
 import time
+import datetime
 import winsound
-import sys
 
 from selenium import webdriver
 
@@ -19,6 +19,8 @@ def auto_handle(mode, lesson_name):
     web_driver.get('https://tsinghua.yuketang.cn/pro/courselist')
     # WebDriver.get('https://yuketang.cn/')
 
+    print(str(datetime.datetime.now()) + "：启动成功")
+
     for cookie in cookies:
         web_driver.add_cookie(cookie)
 
@@ -29,6 +31,9 @@ def auto_handle(mode, lesson_name):
     web_driver.find_element_by_xpath('//*[@id="app"]/div[2]/div[2]/div[3]/div/div[1]/div/div/div/div[2]/button').click()
     time.sleep(5)
     flag = 1
+
+    print(str(datetime.datetime.now()) + "：登录成功")
+
     while flag:
         try:
             # click "正在上课提醒"
@@ -39,7 +44,8 @@ def auto_handle(mode, lesson_name):
                     # 成功发现该课程，发出440Hz、5s的蜂鸣提醒
                     web_driver.find_element_by_xpath('//*[@id="app"]/div[2]/div[2]/div[3]/div/div[1]/div[2]/div').click()
                     winsound.Beep(440, 5000)
-                    flag = 0
+                    print(str(datetime.datetime.now()) + "：进入课程成功")
+                    break
             raise Exception
         except:
             # 否则刷新页面并等待5s
@@ -60,6 +66,7 @@ def auto_handle(mode, lesson_name):
         try:
             # 每隔5s进行检测，发现有题后，发出1kHz、2s的蜂鸣提醒
             web_driver.find_element_by_class_name('page-exercise')
+            print(str(datetime.datetime.now()) + "：发现题目")
             winsound.Beep(1000, 2000)
             time.sleep(5)
         except:
@@ -74,6 +81,7 @@ def auto_handle(mode, lesson_name):
                                              'section/section/section[1]/section/section/section/section/p[1]').click()
             web_driver.find_element_by_xpath('//*[@id="app"]/section/section[1]/section[2]/section/section/section/'
                                              'section[1]/section/section/section/section/div[2]').click()
+            print(str(datetime.datetime.now()) + "：选择题自动答题成功")
         except:
             pass
         # 主观题（有bug）
@@ -88,6 +96,7 @@ def auto_handle(mode, lesson_name):
                 i = i + 1
             web_driver.find_element_by_xpath('//*[@id="app"]/section/section[1]/section[2]/section/section/section/'
                                              'section[1]/section/section/div/p').click()
+            print(str(datetime.datetime.now()) + "：主观题自动答题成功")
         except:
             pass
         # 忘了啥题，应该也是主观题的一种（显然也有问题，实在不行直接把屎山删了吧）
@@ -100,13 +109,17 @@ def auto_handle(mode, lesson_name):
             ).send_keys('不会')
             web_driver.find_element_by_xpath('//*[@id="app"]/section/section[1]/section[2]/section/section/section/'
                                              'section[1]/section/section[2]/div/p').click()
+            print(str(datetime.datetime.now()) + "：主观题自动答题成功")
         except:
             pass
 
 
 if __name__ == '__main__':
-    args = sys.argv
-    if len(args) != 3:
-        print("用法：python run.py [mode] [lesson_name]\n其中：\n    mode        0:自动答题\n                非0:提醒答题\n  "
-              "  lesson_name 课程全名（注意区分课程名中的括号，有些是中文括号，有些是英文括号）")
-    auto_handle(args[1], args[2])
+    import argparse
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--mode", type=bool, default=0, help="运行模式，其中0为自动答题，非0为提醒答题")
+    parser.add_argument("--name", type=str, default="", help="课程全名（注意区分课程名中的括号，有些是中文括号，有些是英文括号）")
+
+    args = parser.parse_args()
+    auto_handle(args.mode, args.name)

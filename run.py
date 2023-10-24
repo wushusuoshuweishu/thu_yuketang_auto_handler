f8297e3ffc1143fea23f090f81ab09239e588c8f
wushusuoshuweishu <djk1160708111@gmail.com>
2023-10-24 18:32:37 +0800

    feat: 支持客观题自动答题

--- run.py
+++ run.py
@@ -1,3 +1,4 @@
+import random
 import time
 import datetime
 import winsound
@@ -7,7 +8,7 @@ from selenium import webdriver
 from config import configs
 
 
-def auto_handle(mode, lesson_name):
+def auto_handle(mode, lesson_name, default_options: list, default_content: str):
     # setup the chrome driver
     web_driver = webdriver.Chrome()
 
@@ -57,56 +58,42 @@ def auto_handle(mode, lesson_name):
     web_driver.switch_to.frame(iframe)
 
     # 手动答题模式
-    while str(mode) != "0":
+    while True:
         try:
             # 每隔5s进行检测，发现有题后，发出1kHz、2s的蜂鸣提醒
             web_driver.find_element('class name', 'page-exercise')
+            if len(web_driver.find_elements('xpath', "//div[@class='timing' and contains(text(), '已完成')]")) != 0:
+                # 该题已答，不用执行下列步骤
+                raise Exception
+            if len(web_driver.find_elements(
+                    'xpath', "//div[contains(@class, 'timing willEnd') and text()='student.receivertimeout']")) != 0:
+                # 该题已超时未答，不用执行下列步骤
+                print(str(datetime.datetime.now()) + "：该题超时未答，当前模式--mode==" + str(mode))
+                raise Exception
             print(str(datetime.datetime.now()) + "：发现题目")
-            winsound.Beep(1000, 2000)
+            if str(mode) != "0":
+                winsound.Beep(1000, 2000)
+            else:
+                elements_with_data_option = web_driver.find_elements('css selector', '[data-option]')
+                if len(elements_with_data_option) > 0:
+                    for element in elements_with_data_option:
+                        data_option_value = element.get_attribute('data-option')
+                        if default_options:
+                            if data_option_value in default_options:
+                                # 依次选择指定的选项
+                                element.click()
+                        else:
+                            # default_options未指定，默认随机选
+                            if random.randint(0, 1):
+                                element.click()
+                else:
+                    # TODO: 主观题自动答题
+                    pass
+                web_driver.find_element("xpath", "//div[@class='slide__shape submit-btn']").click()
             time.sleep(5)
         except:
             time.sleep(5)
             continue
-
-    # TODO:自动答题功能
-    while str(mode) == '0':
-        # 单选题/多选题
-        try:
-            web_driver.find_element_by_xpath('//*[@id="app"]/section/section[1]/section[2]/section/'
-                                             'section/section/section[1]/section/section/section/section/p[1]').click()
-            web_driver.find_element_by_xpath('//*[@id="app"]/section/section[1]/section[2]/section/section/section/'
-                                             'section[1]/section/section/section/section/div[2]').click()
-            print(str(datetime.datetime.now()) + "：选择题自动答题成功")
-        except:
-            pass
-        # 主观题（有bug）
-        try:
-            web_driver.find_element_by_xpath('//*[@id="app"]/section/section[1]/section[2]/section/section/section/'
-                                             'section[1]/section/div/section/section/div[2]').click()
-            i = 1
-            while web_driver.find_element_by_xpath('//*[@id="app"]/section/section[1]/section[2]/section/section/'
-                                                   'section/section[1]/section/section/ul/li[' + str(i) + ']'):
-                web_driver.find_element_by_xpath('//*[@id="app"]/section/section[1]/section[2]/section/section/section/'
-                                                 'section[1]/section/section/ul/li[1]').send_keys('不会')
-                i = i + 1
-            web_driver.find_element_by_xpath('//*[@id="app"]/section/section[1]/section[2]/section/section/section/'
-                                             'section[1]/section/section/div/p').click()
-            print(str(datetime.datetime.now()) + "：主观题自动答题成功")
-        except:
-            pass
-        # 忘了啥题，应该也是主观题的一种（显然也有问题，实在不行直接把屎山删了吧）
-        try:
-            web_driver.find_element_by_xpath('//*[@id="app"]/section/section[1]/section[2]/section/section/section/'
-                                             'section[1]/section/section[1]/div/section/section/div[2]').click()
-            web_driver.find_element_by_xpath(
-                '//*[@id="app"]/section/section[1]/section[2]/section/section/section/section[1]/section/section[2]/'
-                'section/div[2]/section[1]/textarea'
-            ).send_keys('不会')
-            web_driver.find_element_by_xpath('//*[@id="app"]/section/section[1]/section[2]/section/section/section/'
-                                             'section[1]/section/section[2]/div/p').click()
-            print(str(datetime.datetime.now()) + "：主观题自动答题成功")
-        except:
-            pass
 
 
 if __name__ == '__main__':
@@ -115,6 +102,7 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument("--mode", type=str, default=0, help="运行模式，其中0为自动答题，非0为提醒答题")
     parser.add_argument("--name", type=str, default="", help="课程全名（注意区分课程名中的括号，有些是中文括号，有些是英文括号）")
+    parser.add_argument("--options", type=str, default="", help="指定自动答题的选项，例如ABC（单选题则默认选字典序最大者C）。如不指定该参数且mode==0，则随机选项")
 
     args = parser.parse_args()
-    auto_handle(args.mode, args.name)
+    auto_handle(args.mode, args.name, list(args.options), "")
